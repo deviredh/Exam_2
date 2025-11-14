730f2e35e1a6a2397191b0c41ea101b96a2cb3b7
deviredh <deviredh@kean.edu>
2025-11-14 08:10:51 +0000

    FINAL Q2

--- Urbanization_classification.sh
+++ Urbanization_classification.sh
@@ -3,12 +3,13 @@
 #establish path to csv and output text file
 FILE="/home/shared/hasita/Exam_2/Exam2_Levine_et_al_body_size.csv"
 OUT="Urbanization_classification.txt"
+IPSITES="ip_sites.txt"
 
 #add header for output table
 echo -e "Site_Code\tClass" > "$OUT"
 
 #extract a list of the sites and ips, remove any extra characters and duplicates
-tail -n +2 "$FILE" | tr -d '\r' | tr -d '\t' | tr -d ' ' | cut -d',' -f2,5 | sort -u  > ip_sites.txt
+tail -n +2 "$FILE" | tr -d '\r' | tr -d '\t' | tr -d ' ' | cut -d',' -f2,5 | sort -u  > "$IPSITES"
 
 #create a loop for each site and ip class
 while IFS=',' read site ip
@@ -27,7 +28,7 @@ do
 
 	#print results to output table
 	echo -e "$site\t$class" >> "$OUT"
-done < ip_sites.txt
+done < "$IPSITES"
 
 #print to text file
 cat "$OUT"
